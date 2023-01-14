465daf5fb2ae2b0c9c6df7a418bf2b1538cecd20
Sameer Kumar <sameer2001kr@gmail.com>
2023-01-14 19:02:53 +0530

    1 commit

---
+++ Problem_1.cpp
@@ -0,0 +1,27 @@
+/*
+
+242. Valid Anagram
+https://leetcode.com/problems/valid-anagram/description/
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isAnagram(string s, string t)
+{
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+    if (s == t)
+        return true;
+
+    return false;
+}
+
+int main()
+{
+    string s1, s2;
+    cin >> s1;
+    cin >> s2;
+    cout << isAnagram(s1, s2) << endl;
+}
